017d47a71a64a6e0493b5882b48262de788e11ce
Stefan <45903999+sviatil0@users.noreply.github.com>
2025-01-24 15:44:18 -0500

    workign on getting html infromation from the website and parsing it to get a manufacturer name based on the vendor ID

--- EquipmentCheck.ps1
+++ EquipmentCheck.ps1
@@ -48,16 +48,38 @@ Return the object that contains such info
 # https://learn.microsoft.com/en-us/powershell/module/pnpdevice/get-pnpdevice?view=windowsserver2025-ps
 # seems that Generic USB Hub is what hubs should be called
 
-[System.Windows.Forms.MessageBox]::Show("I'm cheking for USB hubs. Please make sure that hub is not plugged in!","Equipment Tracker")
+[System.Windows.Forms.MessageBox]::Show("I'm cheking for USB hubs. Please make sure that a hub is not plugged in!","Equipment Tracker") | Out-Null
 
+Start-Sleep -Seconds 3 
+$initialPnPs = Get-PnpDevice -Class USB -PresentOnly
 
-[System.Windows.Forms.MessageBox]::Show("Plese, plug in a usb hub!","Equipment Tracker")
+[System.Windows.Forms.MessageBox]::Show("Plese, plug in a usb hub!","Equipment Tracker") | Out-Null
+Start-Sleep -Seconds 3 
+$finalPnPs = Get-PnpDevice -Class USB -PresentOnly
+$hubEntries = $finalPnPs | where {$_.InstanceID -notin $initialPnPs.InstanceID}
+echo ($hubEntries[0]).Manufacturer
 
+# Finish getting the vendor from the website
+$vid = "2109"  # Replace with your Vendor ID
+$url = "https://devicehunt.com/search/type/usb/vendor/$vid/device/any"
+
+$response = Invoke-WebRequest -Uri $url -UseBasicParsing
+$manufacturer = ($response.Content -match '<td class="st-val ">')
+
+echo $manufacturer
+
+if ($manufacturer) {
+    Write-Output "Vendor ID: $vid | Manufacturer: $manufacturer"
+} else {
+    Write-Output "Manufacturer not found for Vendor ID: $vid"
+}
 }
 # Main Script
+$trackHubs = [System.Windows.Forms.MessageBox]::show("Any hubs for this computer?","Equipment Tracker","YesNo")
 $monitorsFound = Get-MonitorsStats
 
 if (Test-Path $dataBasePath){
+<#
 $monitorsToAdd = @();
 $monitorsDataBase = Import-Csv -Path $dataBasePath
 $dataBaseSerials = $monitorsDataBase.ManufactureSerial
@@ -67,10 +89,19 @@ foreach ($monitorFound in $monitorsFound){
     if($monitorFound.ManufactureSerial -notin $dataBaseSerials){
         $monitorsToAdd += $monitorFound
     }
+
 }
 $monitorsToAdd | Export-CSV -Path $dataBasePath -NoTypeInformation -Append
+#>
+
+## Tracking the hubs
+
+if($trackHubs){
+    #Tracking hubs
+}
+
 }
 else{
-    $monitorsFound | Export-CSV -Path $dataBasePath -NoTypeInformation
+    # $monitorsFound | Export-CSV -Path $dataBasePath -NoTypeInformation
 }
 
